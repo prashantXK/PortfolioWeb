4679f6f9a163ecec351f42e021236afd76c6268c
PrashantXKratos <prashantxkratos99@gmail.com>
2024-11-15 19:53:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var typeData = new Typed(".role",{
+    strings: [
+        "Web Developer",
+        "Full Stack Web Developer",
+        "Coder",
+    ],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay:1000,
+});
